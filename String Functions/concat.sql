2a5a97333956be8c6bf71c0391401ee0a592ea7f
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-22 21:19:44 -0500

    added concat

---
+++ String Functions/concat.sql
@@ -0,0 +1,28 @@
+SELECT author_fname, author_lname FROM books;
+ 
+SELECT CONCAT('Hello', 'World');
+ 
+SELECT CONCAT('Hello', '...', 'World');
+ 
+SELECT
+  CONCAT(author_fname, ' ', author_lname)
+FROM books;
+ 
+SELECT
+  CONCAT(author_fname, ' ', author_lname)
+  AS 'full name'
+FROM books;
+ 
+SELECT author_fname AS first, author_lname AS last, 
+  CONCAT(author_fname, ' ', author_lname) AS full
+FROM books;
+ 
+SELECT author_fname AS first, author_lname AS last, 
+  CONCAT(author_fname, ', ', author_lname) AS full
+FROM books;
+ 
+SELECT CONCAT(title, '-', author_fname, '-', author_lname) FROM books;
+ 
+SELECT 
+    CONCAT_WS(' - ', title, author_fname, author_lname) 
+FROM books;
