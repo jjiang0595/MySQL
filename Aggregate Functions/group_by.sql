93569028d11c528fad1fada98fe18a173358c3e0
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-23 22:34:38 -0500

    group by cont

--- Aggregate Functions/group_by.sql
+++ Aggregate Functions/group_by.sql
@@ -26,3 +26,25 @@ SELECT CONCAT(author_fname, ' ', author_lname) AS author,  COUNT(*)
 FROM books
 GROUP BY author;
 
+
+-- GROUPING WITH MIN MAX
+SELECT author_lname, MIN(released_year) FROM books GROUP BY author_lname;
+ 
+SELECT author_lname, MAX(released_year), MIN(released_year) FROM books GROUP BY author_lname;
+ 
+SELECT 
+	author_lname, 
+	COUNT(*) as books_written, 
+	MAX(released_year) AS latest_release,
+	MIN(released_year)  AS earliest_release,
+      MAX(pages) AS longest_page_count
+FROM books GROUP BY author_lname;
+ 
+ 
+SELECT 
+	author_lname, 
+        author_fname,
+	COUNT(*) as books_written, 
+	MAX(released_year) AS latest_release,
+	MIN(released_year)  AS earliest_release
+FROM books GROUP BY author_lname, author_fname;
