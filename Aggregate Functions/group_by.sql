d8c30cf6b7c91d50c00dffa28126360e5204f477
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-23 22:34:20 -0500

    group by cont.

--- Aggregate Functions/group_by.sql
+++ Aggregate Functions/group_by.sql
@@ -11,3 +11,18 @@ SELECT
 FROM books
 GROUP BY author_lname
 ORDER BY books_written DESC;
+
+
+-- GROUPING WITH MULTIPLE COLUMNS
+SELECT title, author_fname, author_lname FROM books;
+ 
+SELECT title, author_fname, author_lname FROM books GROUP BY author_lname;
+ 
+SELECT author_fname, author_lname, COUNT(*) FROM books GROUP BY author_lname;
+ 
+SELECT author_fname, author_lname, COUNT(*) FROM books GROUP BY author_lname, author_fname;
+
+SELECT CONCAT(author_fname, ' ', author_lname) AS author,  COUNT(*)
+FROM books
+GROUP BY author;
+
