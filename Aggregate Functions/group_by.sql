4f0f0000212944ad76b1441c7ac235ca12608a7e
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-23 21:30:24 -0500

    add group by

---
+++ Aggregate Functions/group_by.sql
@@ -0,0 +1,13 @@
+SELECT title, author_lname FROM books;
+ 
+SELECT title, author_lname FROM books
+GROUP BY author_lname
+
+SELECT author_lname, COUNT(*) 
+FROM books GROUP BY author_lname;
+
+SELECT 
+    author_lname, COUNT(*) AS books_written
+FROM books
+GROUP BY author_lname
+ORDER BY books_written DESC;
