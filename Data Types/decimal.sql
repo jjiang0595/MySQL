b248d9e70e32d8c5a54cb10116dfe130e9c87cd5
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-24 18:49:25 -0500

    add decimal

---
+++ Data Types/decimal.sql
@@ -0,0 +1,11 @@
+CREATE TABLE products (price DECIMAL(5, 2));
+
+INSERT INTO products (price) VALUES (4.50);
+
+SELECT * FROM products;
+
+INSERT INTO products (price) VALUES (456.99);
+
+INSERT INTO products (price) VALUES (5.026);
+
+SELECT * FROM products;
