456348c0d5d3726763b2c1fd3389129c014ce43b
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-25 21:53:32 -0500

    add less than

---
+++ Logical Operators/less_than.sql
@@ -0,0 +1,7 @@
+SELECT title, released_year FROM books
+WHERE released_year < 2000;
+ 
+SELECT title, released_year FROM books
+WHERE released_year <= 1985;
+
+SELECT title, pages FROM books WHERE pages < 200;
