21d46ec1143b21420a2b949ab065c0a0d9ee1e2e
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-27 14:32:00 -0500

    alter table
    dropping, renaming, modifying columns, and constraints

--- Constraints/multi_col_constraints.sql
+++ Constraints/multi_col_constraints.sql
@@ -21,3 +21,31 @@ ADD COLUMN phone VARCHAR(15);
 
 ALTER TABLE companies
 ADD COLUMN employee_count INT NOT NULL DEFAULT 1;
+
+-- -----------------------------------------------------------------
+
+ALTER TABLE companies DROP COLUMN phone;
+
+-- -----------------------------------------------------------------
+
+RENAME TABLE companies to suppliers;
+
+ALTER TABLE suppliers RENAME TO companies;
+
+ALTER TABLE companies
+RENAME COLUMN name TO company_name;
+
+-- -----------------------------------------------------------------
+
+ALTER TABLE companies
+MODIFY company_name VARCHAR(100) DEFAULT 'unknown';
+
+ALTER TABLE suppliers
+CHANGE business biz_name VARCHAR(50);
+
+-- -----------------------------------------------------------------
+
+ALTER TABLE houses 
+ADD CONSTRAINT positive_pprice CHECK (purchase_price >= 0);
+
+ALTER TABLE houses DROP CONSTRAINT positive_pprice;
