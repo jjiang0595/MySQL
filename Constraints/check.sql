1dd95c02887cedd58e63bb6842ebea4a91c77f64
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-27 14:04:59 -0500

    add check + named constraints

---
+++ Constraints/check.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+    username VARCHAR(20) NOT NULL,
+    age INT CHECK (age >= 0)
+);
+
+CREATE TABLE users2 (
+    username VARCHAR(20) NOT NULL,
+    age INT,
+    CONSTRAINT age_not_negative CHECK (age >= 0)
+);
+
+CREATE TABLE palindromes (
+    word VARCHAR(100) CHECK(REVERSE(word) = word)
+);
+
+CREATE TABLE palindromes2 (
+    word VARCHAR(100),
+    CONSTRAINT word_is_palindrome CHECK(REVERSE(word) = word)
+);
+
