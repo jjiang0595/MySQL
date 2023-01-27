e62ca002d0bcc408c1297671e260450c9b7043ee
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-27 13:42:24 -0500

    add unique

---
+++ Constraints/unique.sql
@@ -0,0 +1,11 @@
+CREATE TABLE contacts (
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE
+)
+
+INSERT INTO contacts (name, phone)
+VALUES ('billybob', '8781213455');
+ 
+-- error
+INSERT INTO contacts (name, phone)
+VALUES ('billybob', '8781213455');
