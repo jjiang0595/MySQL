6e1abe12ef980a78fa6068bbec4ced427c54be41
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-28 14:22:28 -0500

    challenge #3

--- Many to Many/challenge.sql
+++ Many to Many/challenge.sql
@@ -11,3 +11,11 @@ SELECT
 FROM series 
 JOIN reviews ON series.id = reviews.series_id GROUP BY series_id ORDER BY AVG(rating);
 
+-- CHALLENGE #3
+
+SELECT 
+    first_name,
+    last_name,
+    rating 
+FROM reviewers 
+JOIN reviews ON reviewers.id = reviews.reviewer_id;
