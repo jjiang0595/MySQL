5d087e77ebf5201af2d1a21adfd39a23335e3b7f
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-28 14:22:18 -0500

    challenge #2

--- Many to Many/challenge.sql
+++ Many to Many/challenge.sql
@@ -2,3 +2,12 @@
 
 SELECT title, rating FROM series 
 JOIN reviews ON series.id = reviews.series_id;
+
+-- CHALLENGE #2
+
+SELECT 
+    title,
+    ROUND(AVG(rating), 2) AS avg_rating 
+FROM series 
+JOIN reviews ON series.id = reviews.series_id GROUP BY series_id ORDER BY AVG(rating);
+
