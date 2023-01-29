e533388d6b19192654b8bfa5e925336661b35448
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-29 14:19:28 -0500

    dense rank

--- Window Functions/rank.sql
+++ Window Functions/rank.sql
@@ -2,7 +2,10 @@ SELECT
     emp_no, 
     department, 
     salary, 
-    RANK() OVER(PARTITION BY department ORDER BY SALARY) as dept_salary_rank,
-    RANK() OVER(ORDER BY salary DESC) as overall_salary_rank
+    ROW_NUMBER() OVER(PARTITION BY department ORDER BY SALARY DESC) as dept_row_number,
+    RANK() OVER(PARTITION BY department ORDER BY SALARY) AS dept_salary_rank,
+    RANK() OVER(ORDER BY salary DESC) AS overall_rank,
+    DENSE_RANK() OVER(ORDER BY salary DESC) AS overall_salary_dense_rank,
+    ROW_NUMBER() OVER(ORDER BY salary DESC) as overall_num
 FROM employees;
 
