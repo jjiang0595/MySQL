1a5ee81e525d08f6949a84a4791df4d6462f8687
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-29 16:28:06 -0500

    adding users & photos

---
+++ Instagram Clone/ig_clone.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photos (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
