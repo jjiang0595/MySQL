d1d9fca688638cd455b91c1b4493d5db54c4275a
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-29 19:02:34 -0500

    adding comments

--- Instagram Clone/ig_clone.sql
+++ Instagram Clone/ig_clone.sql
@@ -13,3 +13,12 @@ CREATE TABLE photos (
 );
 
 
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (photo_id) REFERENCES photos(id)
+);
