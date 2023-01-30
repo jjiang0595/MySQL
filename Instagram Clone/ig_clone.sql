64d4be69a4a450f028c7516743b306fc3d1f0e8d
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-29 19:22:10 -0500

    adding follows

--- Instagram Clone/ig_clone.sql
+++ Instagram Clone/ig_clone.sql
@@ -28,6 +28,15 @@ CREATE TABLE likes (
     photo_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (user_id) REFERENCES photos(id)
+    FOREIGN KEY (user_id) REFERENCES photos(id),
     PRIMARY KEY(user_id, photo_id)
+);
+
+CREATE TABLE follows (
+    follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (follower_id) REFERENCES users(id),
+    FOREIGN KEY (followee_id) REFERENCES users(id),
+    PRIMARY KEY(followee_id, follower_id)
 );
