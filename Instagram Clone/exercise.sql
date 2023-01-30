aff11f1da5263283522d01a009545059649dbbde
Jerry Jiang <jerryjiang49@gmail.com>
2023-01-29 21:16:30 -0500

    Challenge #2

--- Instagram Clone/exercise.sql
+++ Instagram Clone/exercise.sql
@@ -3,3 +3,12 @@
 SELECT * FROM users ORDER BY created_at LIMIT 5;
 
 
+-- CHALLENGE #2
+
+SELECT 
+    DAYNAME(created_at) AS day,
+    COUNT(*) AS total
+FROM users 
+GROUP BY day
+ORDER BY total DESC
+LIMIT 2;
